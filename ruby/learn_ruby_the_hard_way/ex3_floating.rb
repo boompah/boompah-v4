505405cc2b433a446e94a5fa8d049a9adca14b57
Ryan Bollenbach <ryan@boompah.com>
2013-02-21 00:15:55 -0600

    added floating point work file

---
+++ ruby/learn_ruby_the_hard_way/ex3_floating.rb
@@ -0,0 +1,51 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greather-than
+# <= less-than-equal
+# >= greater-than-equal
+
+# Puts "I will now count my chickens:"
+puts "I will now count my chickens:"
+
+# Puts "Hens" calcuate 25 + 30 / 6
+puts "Hens", 25 + 30 / 6
+
+# Puts "Roosters" calcuate 100 - 25 * 3 % 4
+puts "Roosters", 100 - 25 * 3 % 4
+
+# Puts "Now I will count the eggs:"
+puts "Now I will count the eggs:"
+
+# Calculate 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculate 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7
+
+# Puts "What is 3 + 2?" calcuate 3 + 2
+puts "What is 3 + 2?", 3 + 2
+
+# Puts "What is 5 - 7?" calcuate 5 - 7
+puts "What is 5 - 7?", 5 - 7
+
+# Puts "Oh, that's why it's false." 
+puts "Oh, that's why it's false."
+
+# Puts "How about some more."
+puts "How about some more."
+
+# Puts "Is it greater?" calculate 5 > -2
+puts "Is it greater?", 5 > -2
+
+# Puts "Is it greater or equal?" calculate 5 >= -2
+puts "Is it greater or equal?", 5 >= -2
+
+# Puts "Is it less or equal?" calculate 5 <= -2
+puts "Is it less or equal?", 5 <= -2
