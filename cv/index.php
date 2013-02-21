c26bd6a2574455c0110ee47d69fc942e31704496
Ryan Bollenbach <ryan@boompah.com>
2013-02-20 23:46:11 -0600

    Learn Ruby the Hard Way - lessons 1 - 3

---
+++ cv/index.php
@@ -0,0 +1,25 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/common/header.php";
+   include_once($path);
+?>
+
+<h1 class="home-greeting">Ryan Bollenbach - Curriculum Vitae</h1>
+<h2>UI &amp; UX Design</h2>
+
+<h3>Synopsis</h3>
+
+<p>I’m a strong believer in form following function with design and strive to create interfaces that are easy to use, increase conversions and look beautiful. I’m constantly improving my skill set through research, testing and blogging at boompah.com. I stay on top of new techniques like responsive web design and innovative vertical scrolling layouts. I enjoy being involved with content strategy and copy writing. I’m always reading, growing as a writer and I love creating actionable content with a purpose.</p>
+
+<p>I’m a designer at heart but I also love getting my hands dirty with code. I have a lot of experience with innovate CSS3 techniques, writing validated HTML5, and HAML+SCSS/SASS. I’m comfortable with creating custom responsive Wordpress sites, wordpress security and plugin strategies. I’m constantly learning new things about jQuery and have begun writing my own scripts. I’ve also worked with Zend Framework.</p>
+
+<p>I’ve learned several valuable lessons through my experience at Tungle and Oboxmedia and I thrive in the startup environment. Reaching tight deadlines and having big impacts is one of the most exciting things about seeing a small company grow. My strongest ability is working on web apps. From my recent experience at FBS, I was a lead UX Designer for several enhancements to their MLS search.</p>
+<hr />
+
+
+
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/common/footer.php";
+   include_once($path);
+?>
