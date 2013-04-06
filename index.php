80a076eb41f190b5623f47917b7062efc9bd0747
Ryan Bollenbach <ryan@boompah.com>
2013-04-06 12:54:29 -0500

    moved splash page to root

--- index.php
+++ index.php
@@ -4,32 +4,17 @@
    include_once($path);
 ?>
 
-<h1>Boompah is a User Interface &amp; Development Boutique founded by Ryan Bollenbach and based in Winnipeg, Canada.</h1>
-<p>We collaborate with businesses to design elegant, fresh interfaces for web applications, boutique websites and e-commerce stores. We strive to create timeless designs that perform  beautifully across mobile, tablet and desktop devices. We believe in evidence driven design.</p>
+<div id="logo-circle">
+  <object data="/objects/logo-circle.svg" type="image/svg+xml"></object>
+</div>
 
- <article>
-      <header>
-          <h1>article header h1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
-      </header>
-      <section>
-          <h2>article section h2</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-      </section>
-      <section>
-          <h2>article section h2</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-      </section>
-      <footer>
-          <h3>article footer h3</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
-      </footer>
-  </article>
-
-  <aside>
-      <h3>aside</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
-  </aside>
+<section class="main-content">
+   <h1>Boompah is a User Interface &amp; Development Boutique founded by Ryan Bollenbach and based in Winnipeg, Canada.</h1>
+   <h3>We collaborate with businesses to design elegant, fresh interfaces for web applications,
+   boutique websites and e-commerce stores. We strive to create timeless designs that perform 
+   beautifully across mobile, tablet and desktop devices.
+   We believe in evidence driven design.</h3>
+</div>
 
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
