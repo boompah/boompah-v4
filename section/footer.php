cd91212a85e32a78154efd7c4a9d2b5d63002997
Ryan Bollenbach <ryan@boompah.com>
2013-06-30 23:30:37 -0500

    footer tweaks

--- section/footer.php
+++ section/footer.php
@@ -23,17 +23,16 @@
         <li><a name="footer-menu" href="#top">Top of page <i class="icon-arrow-up"></i></a></li>
         <?php include 'menu-core.php'; ?>
       </ul>
-      <div class="copyright">
-        <p>&copy; Boompah 2006 - <?php echo date("Y") ?>. All rights reserved.</p>
-        <ul class="social-links">
-          <li><a class="icon-twitter" href="https://twitter.com/gotboompah"><span class="screen-reader-text">Boompah on Twitter</span></a></li>
-          <li><a class="icon-facebook" href="https://www.facebook.com/gotboompah"><span class="screen-reader-text">Boompah on Facebook</span></a></li>
-          <li><a href="http://ca.linkedin.com/in/ryanbollenbach" class="icon-linkedin"><span class="screen-reader-text">Boompah on LinkedIn</span></a></li>
-          <li><a class="icon-pinterest" href="http://pinterest.com/boompah/"><span class="screen-reader-text">Boompah on Pinterest</span></a></li>
-          <li><a href="http://boompah.tumblr.com/"><span class="screen-reader-text">Boompah on Tumblr</span></a></li>
-          <li><a href="http://instagram.com/boompah" class=""><span class="screen-reader-text">Boompah on Instagram</span></a></li>
-        </ul>
-      </div><?php // copyright ?>
+      <div class="copyright"></div>
+      <p class="copyright-line">&copy; Boompah 2006 - <?php echo date("Y") ?>. All rights reserved.</p>
+      <ul class="social-links">
+        <li><a class="icon-twitter" href="https://twitter.com/gotboompah"><span class="screen-reader-text">Boompah on Twitter</span></a></li>
+        <li><a class="icon-facebook" href="https://www.facebook.com/gotboompah"><span class="screen-reader-text">Boompah on Facebook</span></a></li>
+        <li><a href="http://ca.linkedin.com/in/ryanbollenbach" class="icon-linkedin"><span class="screen-reader-text">Boompah on LinkedIn</span></a></li>
+        <li><a class="icon-pinterest" href="http://pinterest.com/boompah/"><span class="screen-reader-text">Boompah on Pinterest</span></a></li>
+        <li><a class="icon-tumblr" href="http://boompah.tumblr.com/"><span class="screen-reader-text">Boompah on Tumblr</span></a></li>
+        <li><a class="icon-instagram" href="http://instagram.com/boompah" class=""><span class="screen-reader-text">Boompah on Instagram</span></a></li>
+      </ul>
     </div><?php // container ?>
   </footer><?php // footer ?>
 
