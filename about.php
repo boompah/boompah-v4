0557e50df734e4ba77635c6854e7109ddc9f7576
Ryan Bollenbach <ryan@boompah.com>
2013-07-02 23:01:39 -0500

    added about events

--- about.php
+++ about.php
@@ -21,16 +21,12 @@
 	</div>
 	<div class="row">
 		<div class="half">
-			<h3>Business Life</h3>
-       		<p>Ryan had the opportunity of working with several oustanding international brands including Grooveshark, Cineflix, Reitman's, and ALDO while embracing the french canadian culture. With a passion
-towards electronic music, Ryan was also a regular attendee of Piknic Electronik, Igloofest and other various events.</p>
-      	</div>
-      	<div class="half">
-       		<h3>Personal Life</h3>
-       		<p>After his east coast adventure, Ryan returned to Winnipeg in 2012. He now enjoys golfing, mixing martini's, traveling and is addicted to instagram in his personal life. He also recently began exploring a
-plant-based diet and has quickly become a fan of the vegan lifestyle.</p>
+			<h3>My Timeline</h3>
+       		<p>I'm blessed to have experienced a lot of great things in my life, below are some events I'd like to share with you.</p>
+          <p><em>This concept was inspired by <a href="http://chriscoyier.net/about/" title="About Chris Coyier">Chris Coyier's awesome about page</a></em></p>
       	</div>
     </div><?php // row ?>
+    <!--
     <h3 class="food">Food Porn</h3>
     <p>We love eating healthy food while we work and we're proud of our recipes.</p>
     <div class="row">
@@ -62,5 +58,78 @@ plant-based diet and has quickly become a fan of the vegan lifestyle.</p>
         <p class="cta"><a href="http://hollywoodpq.com" target="_blank" title="Hollywood PQ">Live site</a> <span class="link-split">|</span> <a href="#">Case Study</a></p>
       </div>
     </div><?php // row ?>
+    -->
+  <div class="row">
+    <div class="timeline-bar">
+      <i class="icon-bolt"></i>
+      <span class="date">Nov 1987</span>
+      <h3>Born in Winnipeg, MB.</h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Aug 1991</span>
+      <h3>First day of Kindergarten</h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">May 2004</span>
+      <h3>Graduated High School at Glenlawn Collegiate</h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Aug 2006</span>
+      <h3>Studied Digital Media Technology at Red River College</h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Jun 2008</span>
+      <h3>Moved to Montreal, QC.</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Nov 2009</span>
+      <h3>Kicked off Mograph TV</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">May 2009</span>
+      <h3>Started working at Tungle</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Aug 2010</span>
+      <h3>Kicked off <a href="http://boompah.com" title="Boompah">Boompah.com</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Jan 2010</span>
+      <h3>Started working at Oboxmedia</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Oct 2011</span>
+      <h3>Best man at my best friend's wedding in Hawaii</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Dec 2011</span>
+      <h3>Moved back to Winnipeg, MB.</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Jan 2011</span>
+      <h3>Best man at my brother's wedding in Australia</a></h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Aug 2012</span>
+      <h3>Bought my first set of golf clubs, Top-Flite Tour Edition</h3>
+    </div>
+    <div class="timeline-bar">
+      <i class="icon-building"></i>
+      <span class="date">Oct. 2012</span>
+      <h3>Bought my first car, 2012 Honda Civic</h3>
+    </div>
+  </div>
 </div>
 <?php include'section/footer.php'; ?>
