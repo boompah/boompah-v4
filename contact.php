2b97d36e11a1668e9af748ba89a8bda5c3ca7b45
Ryan Bollenbach <ryan@boompah.com>
2013-07-04 03:15:29 -0500

    contact markup wip

--- contact.php
+++ contact.php
@@ -8,9 +8,38 @@
   <div class="container">
     <section class="main-content">
       <h1>Contact</h1>
-      <h3>Our mission is to collaborate with awesome people that want to make a positive change in the world.
-		Whether it's inspiring a healthy diet, enhancing a wardrobe, or guiding someone through the home buying process.</h3>
+      <h3>In order to make this process as effiecent as possible, please follow the guidelines below before contacting us to discuss a new project.</h3>
     </div>
   </div>
 </div><?php // masthead-wrapper home ?>
+<div class="container">
+  <div class="showcase">
+    <div class="row">
+      <div class="half">
+        <h3>1. Project Breif</h3>
+        <p>Please provide us with as much detail as you can about your project. It’s important for us to ensure it’s a good match and understand how
+         you’d like us to be involved. If you have any wireframes, sketches, notes, objectives or goals, please append those as well.</p>
+      </div>
+      <div class="half">
+        <h3>3. Budget</h3>
+        <p>What is your budget? Once we have an idea of what you’re working with, we can provide insight on what you can do with it.
+         Our typical projects start from $7,500 and our hourly rate is $75.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="half">
+        <h3>2. Timeline</h3>
+        <p>We need to know when a project must begin and end in order to manage expectations. <em>*Please include your timezone, we’re on Central Standard Time.*</em></p>
+      </div>
+      <div class="half">
+        <h3>4. One on One Consultations</h3>
+        <p>We have 2-hour available for individual consultations. Do you need a fresh set of eyes on a project? An actionable list of tweaks and changes? Ideas for new content? Not a problem.
+2-hour UX/Usability review sessions are $250.</p>
+      </div>
+    </div>
+    <div class="row">
+      <h2>Contact Ryan directly for new project enquires.</h2>
+    </div>
+  </div>
+</div>
 <?php include'section/footer.php'; ?>
