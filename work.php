75fb393df5067077e200c067efeffa4d45431da9
Ryan Bollenbach <ryan@boompah.com>
2013-07-06 00:23:56 -0500

    home mobile tweaks

--- work.php
+++ work.php
@@ -4,4 +4,12 @@
   $description = 'Boompah is a User Interface &amp; Development Boutique founded by Ryan Bollenbach and based in Winnipeg, Canada.';
 ?>
 <?php include 'section/header.php'; ?>
+<div id="masthead-wrapper">
+  <div class="container">
+    <section class="main-content">
+      <h1>Work</h1>
+      <h3>Currently in Progress</h3>
+    </div>
+  </div>
+</div><?php // masthead-wrapper home ?>
 <?php include'section/footer.php'; ?>
