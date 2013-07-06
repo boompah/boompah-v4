033514d65b25e5e8c87c90683cff7cb7e4fa321d
Ryan Bollenbach <ryan@boompah.com>
2013-07-06 02:24:14 -0500

    setting up about mobile queries

--- about.php
+++ about.php
@@ -28,64 +28,136 @@
     </div><?php // row ?>
     <div class="row">
       <div class="timeline-bar red__bg">
-        <h3><span class="date">Nov 1987</span>Born in Winnipeg, MB.<i class="icon-bolt"></i></h3>
+        <span class="date">Nov 1987</span>
+        <h3>
+          <span class="event">Born in Winnipeg, MB.</span>
+          <i class="icon-bolt"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Aug 1991</span>First day of Kindergarten<i class="icon-building"></i></h3>
+        <span class="date">Aug 1991</span>
+        <h3>
+          <span class="event">First day of Kindergarten</span>
+          <i class="icon-building"></i>
+        </h3>
       </div>
       <a href="http://skabenya.deviantart.com/" title="Ryan Bollenbach on DeviantArt" target="_blank" class="timeline-bar">
         <div class="timeline-bar deviantart-green__bg">
-          <h3><span class="date">Jan 2002</span>Created a DeviantArt account<i class="icon-picture"></i></h3>
+          <span class="date">Jan 2002</span>
+          <h3>
+            <span class="event">Created a DeviantArt account</span>
+            <i class="icon-picture"></i>
+          </h3>
         </div>
       </a>
       <div class="timeline-bar">
-        <h3><span class="date">May 2004</span>Graduated High School at Glenlawn Collegiate<i class="icon-building"></i></h3>
+        <span class="date">May 2004</span>
+        <h3>
+          <span class="event">Graduated High School at Glenlawn Collegiate</span>
+          <i class="icon-building"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Aug 2006</span>Studied Digital Media Technology at Red River College<i class="icon-building"></i></h3>
+        <span class="date">Aug 2006</span>
+        <h3>
+          <span class="event">Studied Digital Media Technology at Red River College</span>
+          <i class="icon-building"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Jun 2008</span>Moved to Montreal, QC.<i class="icon-globe"></i></h3>
+        <span class="date">Jun 2008</span>
+        <h3>
+          <span class="event">Moved to Montreal, QC.</span>
+          <i class="icon-globe"></i>
+        </h3>
       </div>
       <a href="https://www.facebook.com/mographtv" target="_blank" class="timeline-bar">
         <div class="timeline-bar baby-blue__bg">
-          <h3><span class="date">Nov 2009</span>Kicked off Mograph TV<i class="icon-building"></i></h3>
+          <span class="date">Nov 2009</span>
+          <h3>
+            <span class="event">Kicked off Mograph TV</span>
+            <i class="icon-building"></i>
+          </h3>
         </div>
       </a>
       <div class="timeline-bar tungle-green__bg">
-        <h3><span class="date">May 2009</span>Started working at Tungle</a><i class="icon-calendar"></i></h3>
+        <span class="date">May 2009</span>
+        <h3>
+          <span class="event">Started working at Tungle</span>
+          <i class="icon-calendar"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Aug 2009</span>Bought my first iPhone (3Gs)</a><i class="icon-apple"></i></h3>
+        <span class="date">Aug 2009</span>
+        <h3>
+          <span class="event">Bought my first iPhone (3Gs)</span>
+          <i class="icon-apple"></i>
+        </h3>
       </div>
       <div class="timeline-bar red__bg">
-        <h3><span class="date">Aug 2010</span>Kicked off Boompah.com<i class="icon-building"></i></h3>
+        <span class="date">Aug 2010</span>
+        <h3>
+          <span class="event">Kicked off Boompah.com</span>
+          <i class="icon-building"></i>
+        </h3>
       </div>
       <a href="http://oboxmedia.com" target="_blank" class="timeline-bar" title="Oboxmedia" class="timeline-link">
         <div class="timeline-bar baby-blue__bg">
-          <h3><span class="date">Jan 2010</span>Started working at Oboxmedia<i class="icon-rocket"></i></i></h3>
+          <span class="date">Jan 2010</span>
+          <h3>
+            <span class="event">Started working at Oboxmedia</span>
+            <i class="icon-rocket"></i>
+          </h3>
         </div>
       </a>
       <div class="timeline-bar">
-        <h3><span class="date">Jun 2011</span>Created my Github Account<i class="icon-github"></i></h3>
+        <span class="date">Jun 2011</span>
+        <h3>
+          <span class="event">Created my Github Account</span>
+          <i class="icon-github"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Oct 2011</span>Best man at my best friend's wedding in Hawaii<i class="icon-heart"></i></h3>
+        <span class="date">Oct 2011</span>
+        <h3>
+          <span class="event">Best man at my best friend's wedding in Hawaii</span>
+          <i class="icon-heart"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Dec 2011</span>Moved back to Winnipeg, MB.<i class="icon-globe"></i></h3> 
+        <span class="date">Dec 2011</span>
+        <h3>
+          <span class="event">Moved back to Winnipeg, MB.</span>
+          <i class="icon-globe"></i>
+        </h3> 
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Jan 2011</span>Best man at my brother's wedding in Australia<i class="icon-heart"></i></h3>
+        <span class="date">Jan 2011</span>
+        <h3>
+          <span class="event">Best man at my brother's wedding in Australia</span>
+          <i class="icon-heart"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Aug 2012</span>Bought my first set of golf clubs<i class="icon-thumbs-up"></i></h3>
+        <span class="date">Aug 2012</span>
+        <h3>
+          <span class="event">Bought my first set of golf clubs</span>
+          <i class="icon-thumbs-up"></i>
+        </h3>
       </div>
       <div class="timeline-bar">
-        <h3><span class="date">Oct. 2012</span>Bought my first car, 2012 Honda Civic<i class="icon-truck"></i></h3>
+        <span class="date">Oct. 2012</span>
+        <h3>
+          <span class="event">Bought my first car, 2012 Honda Civic</span>
+          <i class="icon-truck"></i>
+        </h3>
       </div>
       <div class="timeline-bar android__bg">
-        <h3><span class="date">May 2013</span>Started playing with Android, Samsung S4<i class="icon-android"></i></h3>
+        <span class="date">May 2013</span>
+        <h3>
+          <span class="event">Started playing with Android, Samsung S4</span>
+          <i class="icon-android"></i>
+        </h3>
       </div>
     </div>
   </div>
