ceef45e1174f90f840ab6833932e5b7f06faa4ca
Ryan Bollenbach <ryan@boompah.com>
2013-07-07 15:47:52 -0500

    services content - wip - interfaces

---
+++ branding.php
@@ -0,0 +1,73 @@
+<?php
+  $bodyclass = services;
+  $title = 'Services';
+  $description = 'Boompah is a User Interface &amp; Development Boutique founded by Ryan Bollenbach and based in Winnipeg, Canada.';
+?>
+<?php include 'section/header.php'; ?>
+<div id="masthead-wrapper">
+  <div class="container">
+    <section class="main-content">
+      <h1>Services</h1>
+      <h3>We take a holistic approach in creating interfaces and branding to harmonize the user experience of online presences. We incorporate our science of social to create meaningful interactions through engaging content.</h3>
+    </div>
+  </div>
+</div><?php // masthead-wrapper home ?>
+<div class="page-toggle">
+  <div class="container">
+    <div class="left">
+      <h2><a href="/services" title="Interface Design">Interfaces</a></h2>
+    </div>
+    <div class="right">
+      <h2 class="selected">Branding</h2>
+      <div class="arrow-up"></div>
+    </div>
+  </div>
+</div>
+<div class="container showcase services-content">
+  <div class="row">
+    <div class="half">
+      <h3>Web Design</h3>
+      <p>Whether it's starting from scratch, a full on re-design or improving a single page element, we love helping brands through the whole design lifecycle. A web design is usually also be the foundation of a brand’s style guidelines. We constantly strive to refine our process to create solutions that create the best user experience possible.</p>
+      <p>We believe a successful design is the balance of its responsiveness, visual aesthetic, performance and user interaction. We fuel our design decisions with research and testing instead of making assumptions and shooting from the hip.</p>
+    </div>
+    <div class="half">
+      <h3>Style Guides</h3>
+      <p>These days there are several different definitions of a brand style guide and it’s important to keep in mind so your brand stays consistent. Since the majority of marketing is online based, it only makes sense to have a style guide that maps out your online presence.</p>
+      <p>Whether you have an existing website or worked with us on a new or re-designed site, we will document all the characteristics of your brand. This includes specific HTML assets, colour values, typography &amp; fonts, content layouts, forms, graphic styles, responsive web design considerations.</p>
+      <p>The beauty of a style guide is the ability to have multiple designers work on a project while speaking the same language of a brand.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="half">
+      <h3>iAB Advertisements &amp; Social Activation</h3>
+      <p>We create both static and animated banners for running iAB standard ads or custom sizes and specifications. We translate your promotion idea and branding to create a compelling advertisement for potential customers.</p>
+      <p>We can activate or reinvigorate your social media presence by
+  infusing it with your personal brand or promotion(s). Having a following is only the first step in taking full advantage of your audience. Creating a meaningful, personalized connections with your users is a key ingredient to user engagement and retention. We can provide you with out of the box solutions for action oriented content.</p>
+    </div>
+    <div class="half">
+      <h3>Media Kits</h3>
+      <p>From our history of working with sales associates, we understand the value of creating an outstanding media kit to close new deals with your potential clients.</p>
+      <p>Creating an interactive PDF is an excellent way to create a personalized, private experience that will easily work on a ton of devices and screen sizes. Best of all, it’s optimized to print should the need arise.</p>
+      <p>If constant customization is required, we can also provide easy to modify templates through MS Word or Powerpoint.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="half">
+      <h3>Logo Design</h3>
+      <p>A logo is the first step to creating a memorable brand. We can walk you through the process of of assessing the goals and needs of a logo, then create a roadmap of how to approach it. We do not use a fixed process when designs logos but, we try to ask the right questions.</p>
+      <p>What styles of logos and brands inspire you?<br />
+      What do you want the logo to communicate?<br />
+      What fonts do you enjoy?<br />
+      What colours do you enjoy?<br />
+      If you already have a logo, what do you aim to improve upon?<br />
+      What is your target audience?</p>
+    </div>
+    <div class="half">
+      <h3>Business Cards</h3>
+      <p>Business cards will always remain an essential asset to advertise your brand at a one on one level. When you’re at the golf course chatting with a potential client, making a lasting first impression could be the difference between landing a new lead or losing brand respect.</p>
+      <p>We believe it’s good to stay classy but think outside of the box when designing business cards. We’ll ensure brand consistency with your website and help you put your best step forward.</p>
+      <p>We’re affiliated with international and local (Winnipeg, MB) printers to ensure your business cards will be delivered to your doorstep.</p>
+    </div>
+  </div>
+</div>
+<?php include'section/footer.php'; ?>
--- services.php
+++ services.php
@@ -8,65 +8,57 @@
   <div class="container">
     <section class="main-content">
       <h1>Services</h1>
-      <h3>We focus on creating awesome interfaces and out of the box content solutions. The services we offer reflect that.</h3>
+      <h3>We take a holistic approach in creating interfaces and branding to harmonize the user experience of online presences. We incorporate our science of social to create meaningful interactions through engaging content.</h3>
     </div>
   </div>
 </div><?php // masthead-wrapper home ?>
 <div class="page-toggle">
   <div class="container">
     <div class="left">
-      <h2 class="selected">Branding</h2>
+      <h2 class="selected">Interfaces</h2>
       <div class="arrow-up"></div>
     </div>
     <div class="right">
-      <h2><a href="/interface-design" title="Interface Design">Interfaces</a></h2>
+      <h2><a href="/branding" title="Branding">Branding</a></h2>
     </div>
   </div>
 </div>
-<div class="container showcase services-content">
+<div class="container showcase">
   <div class="row">
     <div class="half">
-      <h3>Web Design</h3>
-      <p>Whether it’s starting from scratch, a full on re-design or improving a single asset of a website, we love helping brands through the whole process of improvement. A web design is usually also be the foundation of a brand’s style guidelines. We constantly strive to refine our process to create solutions that create the best user experience possible.</p>
-      <p>We believe a successful design is the balance of its responsiveness, visual aesthetic, performance and user interaction. We fuel our design decisions with research and testing instead of making assumptions and shooting from the hip.</p>
+      <h3>Responsive Web Design</h3>
+      <p>We're constantly refining our responsive web design process, adapting to its steady evolution. Whether it’s a brochure website or intensive web application, we have extensive experience with various ui patterns.</p>
+      <p>We enjoy rocking out with <a href="http://neat.bourbon.io/" title="Bourbon Neat" target="_blank">Neat</a>, <a href="http://twitter.github.io/bootstrap/" title="Twitter's Bootstrap" target="_blank">Bootstrap</a>, and a select handful of responsive frameworks. We also create custom grids and media query architectures from scratch. Our favourite mobile device to design for currently is the Samsung S4.</p>
     </div>
     <div class="half">
-      <h3>Style Guides</h3>
-      <p>These days there are several different definitions of a brand style guide and it’s important to keep in mind so your brand stays consistent. Since the majority of marketing is online based, it only makes sense to have a style guide that maps out your online presence.</p>
-      <p>Whether you have an existing website or worked with us on a new or re-designed site, we will document all the characteristics of your brand. This includes specific HTML assets, color values, typography &amp; fonts, content layouts, forms, graphic styles, responsive web design considerations.</p>
-      <p>The beauty of a style guide is the ability to have multiple designers work on a project while speaking the same language of a brand.</p>
+      <h3>Web Application Design</h3>
+      <p>Working on a web app is the ability to zoom in on a single page element, explore all its states, define its purpose in a workflow while establishing the anatomy of a website. We solve complex usability issues, develop new ideas for functionality and make interfaces pleasurable to use.</p>
+      <p>We've worked on several large scale web apps including advanced calendar software, medical prescriptions organizations, MLS systems and more. We solve solving complex design problems.</p>
     </div>
   </div>
   <div class="row">
     <div class="half">
-      <h3>iAB Advertisements &amp; Social Activation</h3>
-      <p>We create both static and animated banners for running iAB standar ads or custom sizes and specifications. We translate your promotion idea and branding to create a compelling advertisement for potential customers.</p>
-      <p>We can activate or reinvigorate your social media presense by
-  infusing it with your personal brand or promtion(s). Having a following is only the first step in taking full advantage of your audience. Creating a meaningful, personalized connections with your users is a key ingrediant to user engagement and retention. We can provide you with out of the box solutions for action oriented content.</p>
+      <h3>Wireframes &amp; Prototypes</h3>
+      <p>Wireframes are a powerful way conceptualize layouts and quickly iterate. We prefer vector based wireframes created in Adobe’s Illustrator when the goal involves exploring drastically different compositions. We also enjoy <a href="http://www.mockflow.com/" title="Mockflow" target="_blank">Mockflow</a>, <a href="http://uxpin.com/" title="UXPin" target="_blank">UXPin</a>, and <a href="https://gomockingbird.com/" title="Mockingbird" target="_blank">Mockingbird</a>.</p>
+      <p>If the composition’s direction is decided and game plan involves quick prototyping with responsive web design, we recommend responsive HTML based wireframes. 
+        To test out more developed ui's before the final design phase we work with tools like <a href="http://www.invisionapp.com/" title="InVision" target="_blank">InVision</a>,
+         <a href="http://maker.github.io/ratchet/" title="Ratchet" target="_blank">Ratchet</a>, <a href="http://jaunesarmiento.me/fries/" title="Fries" target="_blank">Fries</a>, and <a href="http://foundation.zurb.com/" title="Foundation" target="_blank">Foundation</a>.
+       </p>
     </div>
     <div class="half">
-      <h3>Media Kits</h3>
-      <p>From our history of working with sales associates, we understand the value of creating an outstanding media kit to close new deals with your potential clients.</p>
-      <p>Creating an interactive PDF is an excellent way to create a personalized, private experience that will easily work on a ton of devices and screen sizes. Best of all, it’s optimized to print should the need arise.</p>
-      <p>If constant customization is required, we can also provide easy to modify templates through MS Word or Powerpoint.</p>
+      <h3>E-commerce Stores</h3>
+      <p>We've implemented various e-commerce platforms including Shopify Magento, WooCommerce, Shopify, OpenCart. We’re constantly helping brands find innovative ways to increase and analyze their audience. We're constantly on the hunt for usability findings to evolve our designs based on the success of world leading brands.</p>
     </div>
   </div>
   <div class="row">
     <div class="half">
-      <h3>Logo Design</h3>
-      <p>A logo is the first step to creating a memorable brand. We can walk you through the process of of assessing the goals and needs of a logo, then create a roadmap of how to approach it. We do not use a fixed process when designs logos but, we try to ask the right questions.</p>
-      <p>What styles of logos and brands inspire you?<br />
-      What do you want the logo to communicate?<br />
-      What fonts do you enjoy?<br />
-      What colours do you enjoy?<br />
-      If you already have a logo, what do you aim to improve upon?<br />
-      What is your target audience?</p>
+      <h3>E-mail Campaigns</h3>
+      <p><a href="http://mailchimp.com" title="Mailchimp" target="_blank">Mailchimp</a> is our favourite tool for managing newsletters. We take things to the next level by adding custom graphics and creating landing pages for more information, contests, videos, products etc.</p>
+      <p>We also love writing so, we enjoy collaborating on the overall message and content ideas.</p>
     </div>
     <div class="half">
-      <h3>Business Cards</h3>
-      <p>Business cards will always remain an essential asset to advertise your brand at a one on one level. When you’re at the golf course chatting with a potential client, making a lasting first impression could be the difference between landing a new lead or losing brand respect.</p>
-      <p>We believe it’s good to stay classy but think outside of the box when designing business cards. We’ll ensure brand consistency with your website and help you put your best step forward.</p>
-      <p>We’re affiliated with international and local (Winnipeg, MB) printers to ensure your business cards will be delivered to your doorstep.</p>
+      <h3>Social Media Science</h3>
+      <p>fill out here</p>
     </div>
   </div>
 </div>
