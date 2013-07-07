944b1b9f3b8e4fccb6a09fd9ac67743e4ae81aae
Ryan Bollenbach <ryan@boompah.com>
2013-07-07 00:24:15 -0500

    interface design tweak

--- interface-design.php
+++ interface-design.php
@@ -26,17 +26,6 @@
 <div class="container showcase">
   <div class="row">
     <div class="half">
-      <h3>Wireframes &amp; Prototypes</h3>
-      <p>We believe wireframes are the most powerful when conceptualizing interface designs. We prefer vector based wireframes created in Adobe Illustrator when the goal involves exploring drastically different compositions. If the composition’s direction is decided and game plan involves quick prototyping with responsive web design, we reccomend responsive HTML based wireframes.</p>
-    </div>
-    <div class="half">
-      <h3>Boutique Websites</h3>
-      <p>From our experience of working with several brands in fashion, we’ve adapted to what we call the “boutique design language”. In short, this means creating classy, timliness layouts with clean graphic design, a nice balance of white space and elegant typography.</p>
-      <p>We're very passionate about this approach on many levels. We believe it creates a more specialized apperence, it works well on several budget ranges and adapts to the growth of just about any brand. Boompah.com is a prime example of a boutique website.</p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="half">
       <h3>Responsive Web Design</h3>
       <p>We're constantly refining our responsive web design (RWD) process in order to respond to the vast amount of evolution and innovations that have come out within the last year. Whether it’s a basic borchure website or intensive web application, we have extensive experience in various interface types across each domain.</p>
     </div>
@@ -44,6 +33,17 @@
       <h3>Web Applications</h3>
       <p>Working on web app is the ability to zoom in on a single element of a page and explore all its states and workflow while maintaining and overall view of the entire site and brand. Solving complex workflow issues, making layouts easier to understand and navigate.</p>
       <p>We've worked on several large scale web apps including advanced calendar software, Medical Perscriptions organizations, MLS systems and more. We solve solving complex design problems.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="half">
+      <h3>Wireframes &amp; Prototypes</h3>
+      <p>We believe wireframes are the most powerful when conceptualizing interface designs. We prefer vector based wireframes created in Adobe Illustrator when the goal involves exploring drastically different compositions. If the composition’s direction is decided and game plan involves quick prototyping with responsive web design, we reccomend responsive HTML based wireframes.</p>
+    </div>
+    <div class="half">
+      <h3>Boutique Websites</h3>
+      <p>From our experience of working with several brands in fashion, we’ve adapted to what we call the “boutique design language”. In short, this means creating classy, timliness layouts with clean graphic design, a nice balance of white space and elegant typography.</p>
+      <p>We're very passionate about this approach on many levels. We believe it creates a more specialized apperence, it works well on several budget ranges and adapts to the growth of just about any brand. Boompah.com is a prime example of a boutique website.</p>
     </div>
   </div>
   <div class="row">
