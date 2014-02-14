652b863f66fa40e97ae0d335d60864a240b06e4e
Ryan Bollenbach <ryan@boompah.com>
2014-02-13 22:31:41 -0600

    cv added

--- inc/page.cv.php
+++ inc/page.cv.php
@@ -3,177 +3,181 @@
   $description = 'The Professional History of Ryan Bollenbach';
 ?>
 <div class="container">
-  <h1>Ryan Bollenbach</h1>
-  <h3>Interface Designer &amp; Coder</h3>
-  <p>204 807 0619 // ryan@boompah.com</p>
-  <p><a href="http://boompah.com">boompah.com</a></p>
+  <div class="full-content">
+    <div class="profile-info">
+      <h2>Ryan Bollenbach</h2>
+      <h4>Interface Designer &amp; Coder</h4>
+      <p>204 807 0619 // <a href="mailto:ryan@boompah.com">ryan@boompah.com</a></p>
+      <p><a href="http://boompah.com">boompah.com</a></p>
+    </div>
 
-  <h3>Personal</h3>
+    <h3>Personal</h3>
 
-  <p>I enjoy working independently and I love collaborating with a team. I devour criticism and expect radical honesty when asking for feedback.</p>
-  <p>I have a strong ability to juggle multiple projects and tasks concurrently and handle project/situational changes.</p>
-  <p>I stay abreast of innovations in UX best practices, tools and topics of interest to the UX community and help to infuse them back into projects.</p>
-  <p>Pragmatic, open-minded approach to problem-solving.</p>
+    <p>I enjoy working independently and I love collaborating with a team. I devour criticism and expect radical honesty when asking for feedback.</p>
+    <p>I have a strong ability to juggle multiple projects and tasks concurrently and handle project/situational changes.</p>
+    <p>I stay abreast of innovations in UX best practices, tools and topics of interest to the UX community and help to infuse them back into projects.</p>
+    <p>Pragmatic, open-minded approach to problem-solving.</p>
 
-  <h3>Career Objectives</h3>
+    <h3>Career Objectives</h3>
 
-  <ul>
-    <li>Help brands tell their story through responsive web design, web applications and social media strategy.</li>
-    <li>Grow with a solid team and deliver awesome products.</li>
-    <li>Play a primary role in the UX of iOS or Android applications.</li>
-    <li>Continue to improve remote workflow and collaboration.</li>
-  </ul>
+    <ul>
+      <li>Help brands tell their story through responsive web design, web applications and social media strategy.</li>
+      <li>Grow with a solid team and deliver awesome products.</li>
+      <li>Play a primary role in the UX of iOS or Android applications.</li>
+      <li>Continue to improve remote workflow and collaboration.</li>
+    </ul>
 
-  <h3>Skills</h3>
-  <ul>
-    <li>6 years Wireframes and prototypes - HTML, Illustrator, and wireframing tools</li>
-    <li>6 years UI Design &amp; Custom Graphic Design</li>
-    <li>6 years HTML, CSS, and Basic JS and jQuery</li>
-    <li>2 years Git &amp; Github</li>
-    <li>2 years Familiarity with working on a Ruby on Rails applications</li>
-    <li>ERB or HAML markup</li>
-    <li>Setting up views, controllers and routes.</li>
-    <li>SASS architecture</li>
-    <li>8 years Photoshop, InDesign and Illustrator</li>
-    <li>Various project management tools: Basecamp, Trello, Github and Jira.</li>
-    <li>Strong human factors background, information architecture, human-centered design and user testing including managing UX projects and assigned personnel.</li>
-    <li>Participate in user research to gain deep empathy and understanding of users.</li>
-  </ul>
+    <h3>Skills</h3>
+    <ul>
+      <li>6 years Wireframes and prototypes - HTML, Illustrator, and wireframing tools</li>
+      <li>6 years UI Design &amp; Custom Graphic Design</li>
+      <li>6 years HTML, CSS, and Basic JS and jQuery</li>
+      <li>2 years Git &amp; Github</li>
+      <li>2 years Familiarity with working on a Ruby on Rails applications</li>
+      <li>ERB or HAML markup</li>
+      <li>Setting up views, controllers and routes.</li>
+      <li>SASS architecture</li>
+      <li>8 years Photoshop, InDesign and Illustrator</li>
+      <li>Various project management tools: Basecamp, Trello, Github and Jira.</li>
+      <li>Strong human factors background, information architecture, human-centered design and user testing including managing UX projects and assigned personnel.</li>
+      <li>Participate in user research to gain deep empathy and understanding of users.</li>
+    </ul>
 
-  <h3>Experience</h3>
+    <h3>Experience</h3>
 
-  <h4>Founder &amp; UX Designer</h4>
-  <p>Boompah // boompah.com<br />
-    August 2007 – Present<br />
-    Montreal QC. - Winnipeg, MB.
-  </p>
+    <h4>Founder &amp; UX Designer</h4>
+    <p>Boompah // boompah.com<br />
+      August 2007 – Present<br />
+      Montreal QC. - Winnipeg, MB.
+    </p>
 
-  <p>Boompah is a UI Design &amp; Development Boutique run by Ryan Bollenbach. We collaborate with brands to design interfaces for web applications, boutique websites and e-commerce stores. We thrive on the science of social media and evidence driven design.</p>
+    <p>Boompah is a UI Design &amp; Development Boutique run by Ryan Bollenbach. We collaborate with brands to design interfaces for web applications, boutique websites and e-commerce stores. We thrive on the science of social media and evidence driven design.</p>
 
-  <ul>
-    <li>UI Design and coding for various brands including: ALDO, Cineflix, RW&amp;CO, etc.</li>
-    <li>Social Media Branding &amp; content creation</li>
-    <li>Style Guides, Logo Design, Media Kits, Basic Print Media</li>
-  </ul>
+    <ul>
+      <li>UI Design and coding for various brands including: ALDO, Cineflix, RW&amp;CO, etc.</li>
+      <li>Social Media Branding &amp; content creation</li>
+      <li>Style Guides, Logo Design, Media Kits, Basic Print Media</li>
+    </ul>
 
-  <h4>UX Designer and Front-end Developer</h4>
-  <p>Edison Nation // edisonnation.com<br />
-    July 2012 - Present<br />
-    Charlotte, NC.
-  </p>
+    <h4>UX Designer and Front-end Developer</h4>
+    <p>Edison Nation // edisonnation.com<br />
+      July 2012 - Present<br />
+      Charlotte, NC.
+    </p>
 
-  <p>Edison Nation helps inventors bring their product ideas to market. Edison Nation runs various innovation searches for brands including: PetSmart, Bed Bath &amp; Beyond, Colgate, Pyrex, Clorox, Fisher-Price, Wolfgang Puck, Rubbermaid, Sunbeam, Fender, etc.</p>
+    <p>Edison Nation helps inventors bring their product ideas to market. Edison Nation runs various innovation searches for brands including: PetSmart, Bed Bath &amp; Beyond, Colgate, Pyrex, Clorox, Fisher-Price, Wolfgang Puck, Rubbermaid, Sunbeam, Fender, etc.</p>
 
-  <ul>
-    <li>Contributed to the UX and design to core areas of the application. Worked through various enhancements to the idea submission process, payment system, user profiles and forums.</li>
-    <li>UI quality standards with modular, reusable solutions.</li>
-    <li>Conducted extensive usability testing through the community and public forum.</li>
-    <li>Translated conceptual ideas (business needs and user goals) into elegant, intuitive and engaging experiences.</li>
-    <li>Worked on product sites including As Seen on TV products, Stemage, and Stemlight.</li>
-    <li>Implemented the Medical division’s branding of the brand, Edison Nation Medical</li>
-  </ul>
+    <ul>
+      <li>Contributed to the UX and design to core areas of the application. Worked through various enhancements to the idea submission process, payment system, user profiles and forums.</li>
+      <li>UI quality standards with modular, reusable solutions.</li>
+      <li>Conducted extensive usability testing through the community and public forum.</li>
+      <li>Translated conceptual ideas (business needs and user goals) into elegant, intuitive and engaging experiences.</li>
+      <li>Worked on product sites including As Seen on TV products, Stemage, and Stemlight.</li>
+      <li>Implemented the Medical division’s branding of the brand, Edison Nation Medical</li>
+    </ul>
 
-  <h4>UX Designer</h4>
-  <p>Dynamo // godynamo.com<br />
-    June 2011 – March 2013<br />
-    Montreal, QC.
-  </p>
+    <h4>UX Designer</h4>
+    <p>Dynamo // godynamo.com<br />
+      June 2011 – March 2013<br />
+      Montreal, QC.
+    </p>
 
-  <p>Dynamo is a team of designers, developers &amp; UX specialists. Their client list includes: The Aldo Group, FieldTurf, Cineflix International, Tuango, Yellow Bird Project, ShoeDazzle, Lenbrook International, and Reitmans Canada.</p>
+    <p>Dynamo is a team of designers, developers &amp; UX specialists. Their client list includes: The Aldo Group, FieldTurf, Cineflix International, Tuango, Yellow Bird Project, ShoeDazzle, Lenbrook International, and Reitmans Canada.</p>
 
-  <ul>
-    <li>Independently lead collaborative design sessions with the target audience, interactive designers, technical developers, and project managers to generate superb design work and happy clients.</li>
-    <li>Facilitate brainstorming sessions and conduct stakeholder interviews to elicit business requirements from our clients, addressing product, brand, marketing, content and overall user experience.</li>
-    <li>Helped distribute and prioritize design requests/needs from across the company.</li>
-  </ul>
+    <ul>
+      <li>Independently lead collaborative design sessions with the target audience, interactive designers, technical developers, and project managers to generate superb design work and happy clients.</li>
+      <li>Facilitate brainstorming sessions and conduct stakeholder interviews to elicit business requirements from our clients, addressing product, brand, marketing, content and overall user experience.</li>
+      <li>Helped distribute and prioritize design requests/needs from across the company.</li>
+    </ul>
 
-  <h4>UX Designer</h4>
-  <p>FBS // flexmls.com<br />
-    April 2011 – December 2013<br />
-    Fargo, ND
-  </p>
+    <h4>UX Designer</h4>
+    <p>FBS // flexmls.com<br />
+      April 2011 – December 2013<br />
+      Fargo, ND
+    </p>
 
-  <p>With over 30 years of experience, FBS serves over 120 MLS organizations in the U.S. and internationally with its Flexmls system and its Spark Platform.</p>
+    <p>With over 30 years of experience, FBS serves over 120 MLS organizations in the U.S. and internationally with its Flexmls system and its Spark Platform.</p>
 
-  <ul>
-    <li>Mentor team members and share best practices in areas of design and UX.</li>
-    <li>Support usability process by creating and evolving wireframes and prototypes that were responsive to customer feedback.</li>
-    <li>Work closely with product and engineering teams to ship awesome features.</li>
-    <li>Created documentation for personas, scenarios, information/interaction architectures, wireframes and usability test plans.</li>
-    <li>Communicated with all levels of management and staff to achieve desired outcomes and build strong alliances and relationships across the MLS industry.</li>
-  </ul>
+    <ul>
+      <li>Mentor team members and share best practices in areas of design and UX.</li>
+      <li>Support usability process by creating and evolving wireframes and prototypes that were responsive to customer feedback.</li>
+      <li>Work closely with product and engineering teams to ship awesome features.</li>
+      <li>Created documentation for personas, scenarios, information/interaction architectures, wireframes and usability test plans.</li>
+      <li>Communicated with all levels of management and staff to achieve desired outcomes and build strong alliances and relationships across the MLS industry.</li>
+    </ul>
 
-  <h4>UX Designer</h4>
-  <p>Oboxmedia // oboxmedia.com<br />
-    January 2010 – April 2011<br />
-    Montreal, QC.
-  </p>
+    <h4>UX Designer</h4>
+    <p>Oboxmedia // oboxmedia.com<br />
+      January 2010 – April 2011<br />
+      Montreal, QC.
+    </p>
 
-  <p>Oboxmedia helps develop, target and serve a unique, coveted audience: online, entertainment-conscious Canadians aged between 18 &amp; 44 years old.</p>
+    <p>Oboxmedia helps develop, target and serve a unique, coveted audience: online, entertainment-conscious Canadians aged between 18 &amp; 44 years old.</p>
 
-  <ul>
-    <li>Collaborated with the obox sales team to create media kids to sell impressions, ad units, and custom marketing content packages. Media kits were responsible for 80% of the company’s total revenue.</li>
-    <li>Created IAB compliant and custom ad units for the network’s properties.</li>
-    <li>Worked with internal teams to help shape design process and improve execution.</li>
-    <li>Directed the UX and branding and web design for hollywoodpq.com and <a href="http://doyoulookgood.com" target="_blank">doyoulookgood.com</a>.</li>
-    <li>Created cross network guidelines for templates, modules and ad placements.</li>
-    <li>Worked with Zend Framework and custom Wordpress templates.</li>
-  </ul>
+    <ul>
+      <li>Collaborated with the obox sales team to create media kids to sell impressions, ad units, and custom marketing content packages. Media kits were responsible for 80% of the company’s total revenue.</li>
+      <li>Created IAB compliant and custom ad units for the network’s properties.</li>
+      <li>Worked with internal teams to help shape design process and improve execution.</li>
+      <li>Directed the UX and branding and web design for hollywoodpq.com and <a href="http://doyoulookgood.com" target="_blank">doyoulookgood.com</a>.</li>
+      <li>Created cross network guidelines for templates, modules and ad placements.</li>
+      <li>Worked with Zend Framework and custom Wordpress templates.</li>
+    </ul>
 
-  <h4>Interface Designer</h4>
-  <p>Tungle.me (no longer in opperation)<br />
-    Acquired by Blackberry 04-2011<br />
-    May 2009 – January 2010<br />
-    Montreal, QC.
-  </p>
+    <h4>Interface Designer</h4>
+    <p>Tungle.me (no longer in opperation)<br />
+      Acquired by Blackberry 04-2011<br />
+      May 2009 – January 2010<br />
+      Montreal, QC.
+    </p>
 
-  <p>Tungle.me makes scheduling meetings easy—across organizations, calendar systems and time zones—by eliminating the multiple emails, phone calls and double bookings that typically come with finding a time to meet.</p>
+    <p>Tungle.me makes scheduling meetings easy—across organizations, calendar systems and time zones—by eliminating the multiple emails, phone calls and double bookings that typically come with finding a time to meet.</p>
 
-  <p>Tungle.me synchronizes with leading online calendar systems including Outlook, Google Calendar, Apple iCal, Entourage for Mac, and Lotus Notes, Yahoo! and Windows Live, and does not require visitors to sign in or register to schedule meetings.</p>
+    <p>Tungle.me synchronizes with leading online calendar systems including Outlook, Google Calendar, Apple iCal, Entourage for Mac, and Lotus Notes, Yahoo! and Windows Live, and does not require visitors to sign in or register to schedule meetings.</p>
 
-  <ul>
-    <li>Worked on various public pages of tungle.me to inform users of the product, features, functionality, etc.</li>
-    <li>Worked on various views and workflows of the application including the calendar and user profile.</li>
-    <li>A/B Testing and multivariate testing for both conversion and user action success. This primarily involved the calendar and how users were able to perform tasks such as managing dynamic bookings.</li>
-  </ul>
+    <ul>
+      <li>Worked on various public pages of tungle.me to inform users of the product, features, functionality, etc.</li>
+      <li>Worked on various views and workflows of the application including the calendar and user profile.</li>
+      <li>A/B Testing and multivariate testing for both conversion and user action success. This primarily involved the calendar and how users were able to perform tasks such as managing dynamic bookings.</li>
+    </ul>
 
-  <h4>Web Designer</h4>
-  <p>stradigi formerly known as Pente Group // stradigi.ca<br />
-    June 2008 – April 2009<br />
-    Montreal, QC.
-  </p>
+    <h4>Web Designer</h4>
+    <p>stradigi formerly known as Pente Group // stradigi.ca<br />
+      June 2008 – April 2009<br />
+      Montreal, QC.
+    </p>
 
-  <p>stradigi is a digital agency that provides cutting edge premium digital services for music artists, brands and large scale corporations. stradigi has developed creative solutions for top music artists such as Tiësto, Red Foo and the LaFreak Crew, Goonrock, Will.I.AM and more.</p>
+    <p>stradigi is a digital agency that provides cutting edge premium digital services for music artists, brands and large scale corporations. stradigi has developed creative solutions for top music artists such as Tiësto, Red Foo and the LaFreak Crew, Goonrock, Will.I.AM and more.</p>
 
-  <ul>
-    <li>Participated in brainstorming sessions with various clients and translated business goals into interactive experiences.</li>
-    <li>Managed other designers and developers through various client’s website projects.</li>
-    <li>HTML, CSS, Javascript, and jQuery</li>
-  </ul>
+    <ul>
+      <li>Participated in brainstorming sessions with various clients and translated business goals into interactive experiences.</li>
+      <li>Managed other designers and developers through various client’s website projects.</li>
+      <li>HTML, CSS, Javascript, and jQuery</li>
+    </ul>
 
-  <h3>Education</h3>
+    <h3>Education</h3>
 
-  <h4>Digital Multimedia Technology<br />
-    (aka. Digital Media Design)
-  </h4>
+    <h4>Digital Multimedia Technology<br />
+      (aka. Digital Media Design)
+    </h4>
 
-  <p>Red River College, Winnipeg MB.<br />
-    September 2006 – May 2008<br />
-    Bachelor’s degree<br />
-    Hours list Award for receiving a GPA above 4.0
-  </p>
+    <p>Red River College, Winnipeg MB.<br />
+      September 2006 – May 2008<br />
+      Bachelor’s degree<br />
+      Hours list Award for receiving a GPA above 4.0
+    </p>
 
-  <p>This program provides up-to-date training in web and mobile design and development, digital video, 3D animation, motion graphics, and graphic design.</p>
-  <p>It teaches both design fundamentals and technical skills, and encourages creativity, imagination, professionalism, and a strong work ethic.</p>
+    <p>This program provides up-to-date training in web and mobile design and development, digital video, 3D animation, motion graphics, and graphic design.</p>
+    <p>It teaches both design fundamentals and technical skills, and encourages creativity, imagination, professionalism, and a strong work ethic.</p>
 
-  <p><strong>Topics Studied:</strong></p>
-  <ul>
-    <li>Graphic design fundamentals</li>
-    <li>Web Design - Photoshop, Illustrator, InDesign
-    <li>Web Development - HTML, CSS, JS, PHP</li>
-    <li>UX Design</li>
-    <li>Typography</li>
-    <li>Traditional drawing as well as digital drawing techniques</li>
-    <li>Video Production and Motion Graphics</li>
-  </li>
+    <p><strong>Topics Studied:</strong></p>
+    <ul>
+      <li>Graphic design fundamentals</li>
+      <li>Web Design - Photoshop, Illustrator, InDesign</li>
+      <li>Web Development - HTML, CSS, JS, PHP</li>
+      <li>UX Design</li>
+      <li>Typography</li>
+      <li>Traditional drawing as well as digital drawing techniques</li>
+      <li>Video Production and Motion Graphics</li>
+    </ul>
+  </div>
 </div>
