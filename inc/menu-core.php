5f3e22df7745850461a258c914c2441b367e996b
Ryan Bollenbach <ryan@boompah.com>
2014-02-14 15:59:33 -0600

    updated menu order

--- inc/menu-core.php
+++ inc/menu-core.php
@@ -1,5 +1,5 @@
 <li><a href="/" title="Home">Home</a></li>
-<li><a href="http://blog.boompah.com" title="Blog">Blog</a></li>
-<li><a href="?page=about" title="About">About</a></li>
 <li><a href="?page=services" title="Services">Services</a></li>
+<li><a href="?page=about" title="About">About</a></li>
+<li><a href="http://blog.boompah.com" title="Blog">Blog</a></li>
 <li><a href="?page=contact" title="Contact">Contact</a></li>
