e8e069a7d856088a7a255a79797ca8af294a93a6
Ryan Bollenbach <ryan@boompah.com>
2014-06-07 11:36:54 -0500

    kicked off learn something new everyday

---
+++ learn.php
@@ -0,0 +1,29 @@
+<?php
+$page = learn;
+$title = 'Learn Something New Everyday';
+$description = 'Boompah is a User Interface &amp; Development Boutique founded by Ryan Bollenbach and based in Winnipeg, Canada.';
+?>
+<?php include 'inc/header.php' ?>
+<div class="container">
+  <h1>Learn Something New Everyday</h1>
+  <p> On July 6th 2014 I decided to take on the challenge to learn something new everyday for one year <em>(at least).</em> Regardless of how big or small, I'm using this page to track articles I read and things I learn. In parallel to learning, I plan to implement things I learn into the development of this site! Fun stuff.</p>
+  <h3>Desired Topics</h3>
+  <p>SCSS, HTML, Social Media, Javascript, Angular, UX Design, Mobile App Design</p>
+  <h3>06.07.2014 - SASS Learning 02</h3>
+  <ul>
+    <li>
+      <strong>SASS</strong> - <a href="http://thesassway.com/intermediate/understanding-placeholder-selectors" title="Understanding placeholder selectors">Understanding placeholder selectors</a>
+      <ul>
+        <li>Limitations: <code>@extend</code> applied to placeholder selectors doesn't work between rules in different <code>@media</code> blocks</li>
+      </ul>
+    </li>
+  </ul>
+  <h3>06.06.2014 - SASS Learning 01</h3>
+  <ul>
+    <li><strong>SASS</strong> - Refreshing myself with mixins, extend / inheritence, and operators</li>
+    <li><strong>SASS</strong> - Using semantic variable names - <a href="http://thesassway.com/beginner/variable-naming" title="Choosing great variable names">source</a></li>
+    <li><strong>SASS</strong> - <a href="http://thesassway.com/beginner/how-to-structure-a-sass-project" title="How to structure a SASS project">How to structure a SASS project</a></li>
+    <li><strong>SASS</strong> - <a href="http://thesassway.com/beginner/the-inception-rule" title="Nested selectors: the inception rule">Nested selectors: the inception rule</a></li>
+  </ul>
+</div>
+<?php include 'inc/footer.php' ?>
