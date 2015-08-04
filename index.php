7ee30db10e05075bd57aa2b04fc4ee1dad15d8e8
Ryan Bollenbach <ryan@boompah.com>
2015-08-04 00:19:05 -0500

    added twiddy

--- index.php
+++ index.php
@@ -16,69 +16,49 @@ $description = 'Boompah is the UI/UX portfolio and consultancy of Ryan Bollenbac
 </div><?php // masthead-wrapper home ?>
 <div class="showcase featured-work">
   <div class="container">
-    <h2>Featured Work</h2>
+    <h2>Portfolio</h2>
     <div class="row">
-      <div class="panel">
-        <a href="http://diamond.com" target="_blank" title="Diamond.com - Jewelry Education">
-          <div class="panel-block diamond">
-            <img src="/img/featured-work/diamond.png" alt="Diamond" title="Diamond" />
-          </div>
-        </a>
-        <p><a href="http://ice.com" target="_blank" title="Online Jewelry Shop"></a></p>
+      <div class="full-span-portfolio-item">
+        <img>
+        <a></a>
       </div>
-      <div class="panel">
-        <a href="http://2m2.ca" target="_blank" title="2M2, Montreal Art Collective">
-          <div class="panel-block two-m-two">
-            <img src="/img/featured-work/2m2.png" alt="2m2" title="2m2" />
-          </div>
-        </a>
-        <p><a href="http://2m2.ca" target="_blank" title="2M2, Montreal Art Collective"></a></p>
-      </div>
-      <div class="panel last">
-        <a href="http://hollywoodpq.com" target="_blank" title="Hollywood PQ">
-          <div class="panel-block hollywood-pq">
-            <img src="/img/featured-work/hollywoodpq.png" alt="Hollywood PQ" title="Hollywood PQ" />
-          </div>
-        </a>
-        <p><a href="http://2m2.ca" target="_blank" title="2M2, Montreal Art Collective"></a></p>
-      </div>
-    </div><?php // row ?>
-  </div><?php // container ?>
-</div><?php // showcase ?>
+    </div>
+  </div>
+</div>
 <div class="showcase gray-bg hidden-mobile">
   <div class="container">
     <h2>Previous Clients</h2>
     <div class="row">
       <div class="clients">
         <ul>
-          <li><a href="http://www.edisonnation.com" target="_blank" class="btn edison-nation">Edison Nation<i class="icon-bolt"></i></a></li>
-          <li><a href="http://oboxmedia.com/" target="_blank" class="btn baby-blue__bg">Oboxmedia<i class="icon-rocket"></i></a></li>
-          <li><a href="http://doyoulookgood.com/index/home?lang=en" target="_blank" class="btn orange">Doyoulookgood<i class="icon-group"></i></a></li>
-          <li><a href="http://hollywoodpq.com/" target="_blank" class="btn hollywoodpq">Hollywood PQ<i class="icon-comments"></i></a></li>
+          <li>Edison Nation<i class="icon-bolt"></i></li>
+          <li>Oboxmedia<i class="icon-rocket"></i></li>
+          <li>Doyoulookgood<i class="icon-group"></i></li>
+          <li>Hollywood PQ<i class="icon-comments"></i></li>
         </ul>
       </div><?php // one-fourth ?>
       <div class="clients">
         <ul>
-          <li><a href="http://www.aldoshoes.com/ca-eng" target="_blank" class="btn">ALDO<i class="icon-shopping-cart"></i></a></li>
-          <li><a href="http://momindustries.com" target="_blank" class="btn">Mom Industries<i class="icon-picture"></i></a></li>
-          <li><a href="http://www.fieldturf.com/en/" target="_blank" class="btn field-turf">Field Turf<i class="icon-sun"></i></a></li>
-          <li><a href="https://www.flexmls.com/" target="_blank" class="btn fbs">FlexMLS<i class="icon-home"></i></a></li>
+          <li>ALDO<i class="icon-shopping-cart"></i></li>
+          <li>Mom Industries<i class="icon-picture"></i></li>
+          <li>Field Turf<i class="icon-sun"></i></li>
+          <li>FlexMLS<i class="icon-home"></i></li>
         </ul>
       </div><?php // one-fourth ?>
       <div class="clients">
         <ul>
-          <li><a href="http://www.godynamo.com/" target="_blank" class="btn">Dynamo<i class="icon-picture"></i></a></li>
-          <li><a href="http://www.reitmans.com/" target="_blank" class="btn reitmans">Reitmans<i class="icon-female"></i></a></li>
-          <li><a href="http://techcrunch.com/2012/09/18/rim-to-shut-down-tungle-me-team-will-focus-on-blackberry-10-calendar-app/" target="_blank" class="btn tungle-me">Tungle.me<i class="icon-calendar"></i></a></li>
-          <li><a href="http://www.cineflix.com/" target="_blank" class="btn royal-blue">Cineflix<i class="icon-film"></i></a></li>
+          <li>Dynamo<i class="icon-picture"></i></li>
+          <li>Reitmans<i class="icon-female"></i></li>
+          <li>Tungle.me<i class="icon-calendar"></i></li>
+          <li>Cineflix<i class="icon-film"></i></li>
         </ul>
       </div><?php // one-fourth ?>
       <div class="clients">
         <ul>
-          <li><a href="http://www.yellowbirdproject.com/" target="blank" class="btn ybp">Yellow Bird Project<i class="icon-music"></i></a></li>
-          <li><a href="http://www.liberte.ca/en/nutrition-health/" target="_blank" class="btn">Liberte Nutrition<i class="icon-heart"></i></a></li>
-          <li><a href="http://www.diamond.com/" target="_blank" class="btn diamond">Diamond.com<i class="icon-female"></i></a></li>
-          <li><a href="http://fellerath.com/" target="_blank" class="btn fellerath">Kate Fellerath<i class="icon-camera-retro"></i></a></li>
+          <li>Yellow Bird Project<i class="icon-music"></i></li>
+          <li>Liberte Nutrition<i class="icon-heart"></i></li>
+          <li>Diamond.com<i class="icon-female"></i></li>
+          <li>Kate Fellerath<i class="icon-camera-retro"></i></li>
         </ul>
       </div><?php // one-fourth ?>
     </div><?php // row ?>
